aaec40a313757612c939d3cb80fbd2ec36abe9bf
sebastianiv21 <seluti98@gmail.com>
2023-11-16 20:19:29 -0500

    hello world in typescript

---
+++ app.js
@@ -0,0 +1,8 @@
+"use strict";
+const message = "Hello World";
+const hero = {
+    name: "Ironman",
+    age: 45,
+};
+hero.age = 50;
+console.log(hero.age + 1);
---
+++ app.ts
@@ -0,0 +1,10 @@
+const message: string = "Hello World";
+
+const hero = {
+  name: "Ironman",
+  age: 45,
+};
+
+hero.age = 50;
+
+console.log(hero.age + 1);
